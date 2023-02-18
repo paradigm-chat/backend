0b9f139213e764d2622cca09a659231d369e1f6a
amirreza <amir.rezakheradmand.2017@gmail.com>
2023-02-18 15:48:43 +0330

    add: improve RepositoryServiceProvider with registration repositories in loop

--- app/Providers/RepositoryServiceProvider.php
+++ app/Providers/RepositoryServiceProvider.php
@@ -8,10 +8,23 @@ use App\Repositories\Message\MessageRepository;
 use App\Repositories\Message\MessageRepositoryInterface;
 use App\Repositories\User\UserRepository;
 use App\Repositories\User\UserRepositoryInterface;
+use Illuminate\Support\Facades\App;
 use Illuminate\Support\ServiceProvider;
 
 class RepositoryServiceProvider extends ServiceProvider
 {
+
+    /**
+     * The repositories handler mappings for registration in the application.
+     *
+     * @var array<string, string>
+     */
+    protected array $repositories = [
+        UserRepositoryInterface::class => UserRepository::class,
+        ChatRepositoryInterface::class => ChatRepository::class,
+        MessageRepositoryInterface::class => MessageRepository::class,
+    ];
+
     /**
      * Register services.
      *
@@ -19,9 +32,11 @@ class RepositoryServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        $this->app->bind(UserRepositoryInterface::class, UserRepository::class);
-        $this->app->bind(ChatRepositoryInterface::class, ChatRepository::class);
-        $this->app->bind(MessageRepositoryInterface::class, MessageRepository::class);
+        $this->booting(function () {
+            foreach ($this->getRepositories() as $abstract => $concrete) {
+                App::bind($abstract, $concrete);
+            }
+        });
     }
 
     /**
@@ -32,5 +47,13 @@ class RepositoryServiceProvider extends ServiceProvider
     public function boot()
     {
         //
+    }
+
+    /**
+     * @return array
+     */
+    public function getRepositories(): array
+    {
+        return $this->repositories;
     }
 }
